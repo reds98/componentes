0eb83bd723bc87fd71b2d76cfb0f4fef835b4697
Sahid Rojas Chacon <sahid.rojas64@gmail.com>
2025-11-12 19:35:35 -0600

    Initial commit

---
+++ src/Anuncio.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import './Anuncio.css';
+
+function Anuncio() {
+
+
+  return (
+    <div>
+      <h2 className="titulitoAzul">¡Anuncio!</h2>
+      <p className="parrafoRojo">Este es un anuncio importante.</p>
+    </div>
+  );
+}
+
+export default Anuncio;
---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Anuncio from './Anuncio.jsx'
+
+function App() {
+
+
+  return (
+    <div>
+      <h1>Hola hoy veremos componentes</h1>
+      <Anuncio />
+      <Anuncio />
+      <Anuncio />
+    </div>
+  )
+}
+
+export default App
